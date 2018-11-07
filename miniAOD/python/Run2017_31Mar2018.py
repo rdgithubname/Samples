5ef91989c1e6dcbe633f2844668ba708287c4a80
Daniel Spitzbart <daniel.spitzbart@cern.ch>
2018-11-07 14:51:33 +0100

    data samples

---
+++ miniAOD/python/Run2017_31Mar2018.py
@@ -0,0 +1,20 @@
+from Samples.Tools.Sample import Sample
+
+## DoubleMuon
+DoubleMuon_Run2017B_31Mar2018   = Sample("DoubleMuon_Run2017B_31Mar2018", "/DoubleMuon/Run2017B-31Mar2018-v1/MINIAOD")
+DoubleMuon_Run2017C_31Mar2018   = Sample("DoubleMuon_Run2017C_31Mar2018", "/DoubleMuon/Run2017C-31Mar2018-v1/MINIAOD")
+DoubleMuon_Run2017D_31Mar2018   = Sample("DoubleMuon_Run2017D_31Mar2018", "/DoubleMuon/Run2017D-31Mar2018-v1/MINIAOD")
+DoubleMuon_Run2017E_31Mar2018   = Sample("DoubleMuon_Run2017E_31Mar2018", "/DoubleMuon/Run2017E-31Mar2018-v1/MINIAOD")
+DoubleMuon_Run2017F_31Mar2018   = Sample("DoubleMuon_Run2017F_31Mar2018", "/DoubleMuon/Run2017F-31Mar2018-v1/MINIAOD")
+
+DoubleMuon = [
+    DoubleMuon_Run2017B_31Mar2018,
+    DoubleMuon_Run2017C_31Mar2018,
+    DoubleMuon_Run2017D_31Mar2018,
+    DoubleMuon_Run2017E_31Mar2018,
+    DoubleMuon_Run2017F_31Mar2018,
+]
+
+## sum up
+allSamples = DoubleMuon
+
