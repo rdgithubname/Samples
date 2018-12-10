e24d6001045250097613c6ebf1d31dd4b8f4ace6
Daniel Spitzbart <daniel.spitzbart@cern.ch>
2018-12-10 11:10:47 +0100

    Autumn18 relVals for HEM studies

---
+++ miniAOD/python/Autumn18_miniAODv1.py
@@ -0,0 +1,22 @@
+'''
+miniAOD samples of Autumn18 campaign, miniAOD (102X)
+Get the full list of samples with
+dasgoclient -query="dataset=/*/RunIIAutumn18*/MINIAODSIM"
+'''
+
+from Samples.Tools.Sample import Sample
+
+RelValTTbar                 = Sample("RelValTTbar", "/RelValTTbar_13/CMSSW_10_2_4-PU25ns_102X_upgrade2018_realistic_v12-v1/MINIAODSIM")
+RelValTTbar_HEM             = Sample("RelValTTbar_HEM", "/RelValTTbar_13/CMSSW_10_2_4-PU25ns_102X_upgrade2018_realistic_v12HEfail_v1-v1/MINIAODSIM")
+RelValTTbar_HEMmitigation   = Sample("RelValTTbar_HEMmitigation", "/RelValTTbar_13/CMSSW_10_2_4-PU25ns_102X_upgrade2018_realistic_v12HEfail_v1_badHcalMitig-v1/MINIAODSIM")
+
+relvals = [
+    RelValTTbar,
+    RelValTTbar_HEM,
+    RelValTTbar_HEMmitigation,
+]
+
+
+
+
+allSamples = relvals
