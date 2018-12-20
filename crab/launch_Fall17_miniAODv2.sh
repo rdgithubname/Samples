fc2062cc5b3a0162103596b8d7d482fbbf46b4da
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2018-12-20 19:51:09 +0100

    small

--- crab/launch_Fall17_miniAODv2.sh
+++ crab/launch_Fall17_miniAODv2.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
 # DY
-python launch.py --config mc_94X_Fall17_miniAODv2  --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Fall17_miniAODv2   --sample  DY
+python launch.py $@ --config mc_94X_Fall17_miniAODv2  --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Fall17_miniAODv2   --sample  DY
 
-## single top
-#python launch.py --config mc_94X_Fall17_miniAODv2  --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Fall17_miniAODv2   --sample  ST_schannel_4f_amc_F17_94X
-#python launch.py --config mc_94X_Fall17_miniAODv2  --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Fall17_miniAODv2   --sample  ST_5f
-#
-## TT
-#python launch.py --config mc_94X_Fall17_miniAODv2  --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Fall17_miniAODv2   --sample  TTJets_semilepFromT_LO_F17_94X
-#python launch.py --config mc_94X_Fall17_miniAODv2  --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Fall17_miniAODv2   --sample  TTJets_semilepFromTbar_LO_F17_94X
-#python launch.py --config mc_94X_Fall17_miniAODv2  --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Fall17_miniAODv2   --sample  TTJets_dilep_LO_F17_94X
-#python launch.py --config mc_94X_Fall17_miniAODv2  --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Fall17_miniAODv2   --sample  TTTo2L2Nu_pow_F17_94X
-#python launch.py --config mc_94X_Fall17_miniAODv2  --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Fall17_miniAODv2   --sample  TTTo2L2Nu_pow_PS_F17_94X
-#python launch.py --config mc_94X_Fall17_miniAODv2  --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Fall17_miniAODv2   --sample  TTToSemiLeptonic_pow_F17_94X
-#python launch.py --config mc_94X_Fall17_miniAODv2  --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Fall17_miniAODv2   --sample  TTToSemiLeptonic_pow_PS_F17_94X
-#
-## rest
-#python launch.py --config mc_94X_Fall17_miniAODv2  --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Fall17_miniAODv2   --sample  TTX
-#python launch.py --config mc_94X_Fall17_miniAODv2  --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Fall17_miniAODv2   --sample  TTVV
-#python launch.py --config mc_94X_Fall17_miniAODv2  --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Fall17_miniAODv2   --sample  WJets
-#python launch.py --config mc_94X_Fall17_miniAODv2  --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Fall17_miniAODv2   --sample  diboson
-#python launch.py --config mc_94X_Fall17_miniAODv2  --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Fall17_miniAODv2   --sample  multiboson
-#python launch.py --config mc_94X_Fall17_miniAODv2  --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Fall17_miniAODv2   --sample  gluglu
+# single top
+python launch.py $@ --config mc_94X_Fall17_miniAODv2  --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Fall17_miniAODv2   --sample  ST_schannel_4f_NLO_F17_94X
+python launch.py $@ --config mc_94X_Fall17_miniAODv2  --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Fall17_miniAODv2   --sample  ST_5f
+
+# TT
+python launch.py $@ --config mc_94X_Fall17_miniAODv2  --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Fall17_miniAODv2   --sample  TTJets_semilepFromT_LO_F17_94X
+python launch.py $@ --config mc_94X_Fall17_miniAODv2  --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Fall17_miniAODv2   --sample  TTJets_semilepFromTbar_LO_F17_94X
+python launch.py $@ --config mc_94X_Fall17_miniAODv2  --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Fall17_miniAODv2   --sample  TTJets_dilep_LO_F17_94X
+python launch.py $@ --config mc_94X_Fall17_miniAODv2  --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Fall17_miniAODv2   --sample  TTTo2L2Nu_pow_F17_94X
+python launch.py $@ --config mc_94X_Fall17_miniAODv2  --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Fall17_miniAODv2   --sample  TTTo2L2Nu_pow_PS_F17_94X
+python launch.py $@ --config mc_94X_Fall17_miniAODv2  --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Fall17_miniAODv2   --sample  TTToSemiLeptonic_pow_F17_94X
+python launch.py $@ --config mc_94X_Fall17_miniAODv2  --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Fall17_miniAODv2   --sample  TTToSemiLeptonic_pow_PS_F17_94X
+
+# rest
+python launch.py $@ --config mc_94X_Fall17_miniAODv2  --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Fall17_miniAODv2   --sample  TTX
+python launch.py $@ --config mc_94X_Fall17_miniAODv2  --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Fall17_miniAODv2   --sample  TTVV
+python launch.py $@ --config mc_94X_Fall17_miniAODv2  --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Fall17_miniAODv2   --sample  WJets
+python launch.py $@ --config mc_94X_Fall17_miniAODv2  --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Fall17_miniAODv2   --sample  diboson
+python launch.py $@ --config mc_94X_Fall17_miniAODv2  --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Fall17_miniAODv2   --sample  multiboson
+python launch.py $@ --config mc_94X_Fall17_miniAODv2  --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Fall17_miniAODv2   --sample  gluglu
