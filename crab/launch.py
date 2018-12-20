8af2d36fac89538d11a44cc039d82e995933e014
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2018-12-20 15:15:03 +0100

    multi submission

--- crab/launch.py
+++ crab/launch.py
@@ -22,6 +22,9 @@ from Samples.miniAOD.Run2018_promptReco import allSamples as Run2018_promptReco
 # 2018 rereco Data (for A->C)
 from Samples.miniAOD.Run2018_17Sep2018  import allSamples as Run2018_17Sep2018
 
+all_modules = [ "Spring16_miniAODv2", "Summer16_miniAODv2", "Summer16_miniAODv3", "Fall17_miniAODv2", "Autumn18_miniAODv1"]
+all_modules +=[ "Run2016_17Jul2018", "Run2017_31Mar2018", "Run2018_26Sep2018", "Run2018_promptReco", "Run2018_17Sep2018"]
+
 allSamples  = Spring16_miniAODv2 + Summer16_miniAODv2 + Summer16_miniAODv3 + Fall17_miniAODv2 + Autumn18_miniAODv1
 allSamples += Run2016_17Jul2018 + Run2017_31Mar2018 + Run2018_26Sep2018 + Run2018_promptReco + Run2018_17Sep2018
 
@@ -31,82 +34,84 @@ parser.add_option("--production_label", dest="production_label",        help="pr
 parser.add_option("--remoteDir",        dest="remoteDir",               help="remote subdirectory", default="")
 parser.add_option("--unitsPerJob",      dest="unitsPerJob",             help="Nr. of units (files) / crab job", type="int", default=1)
 parser.add_option("--totalUnits",       dest="totalUnits",              help="Total nr. of units (files)", type="int", default=None)
-parser.add_option("--era",              dest="era",                     help="Which era?")
+parser.add_option("--config",           dest="config",                     help="Which config?")
+parser.add_option("--module",           dest="module",                  choices = all_modules, help="Which module X in Samples.miniAOD.X?")
 parser.add_option("--sample",           dest="sample",                  help="Which sample?")
 parser.add_option("--publish",          action='store_true',            help="Publish on dbs?", default=False)
 parser.add_option("--dryrun",           action='store_true',            help="Test script?", default=False)
 ( options, args ) = parser.parse_args()
 
-print "## Starting submission to crab for sample %s"%options.sample
+print "## Starting submission to crab for sample(s) %s from module %s"%( options.sample, options.module )
 
-dataset = None
-for sample in allSamples:
-    if sample.name == options.sample:
-        dataset = sample.DASname
+module_file = os.path.expandvars( "$CMSSW_BASE/python/Samples/miniAOD/%s.py" % options.module )
+try:
+    tmp_module = imp.load_source( options.sample, os.path.expandvars( module_file ) ) 
+    datasets   = getattr(tmp_module, options.sample)
+except:
+    raise RuntimeError( "Not found: Sample: %s, Module: %s, module_file: %s" % (options.sample, options.module, module_file) ) 
 
-if dataset is None:
-    raise NotImplementedError
+if type(datasets)!=type([]):
+    datasets = [datasets]
 
-print "## Found dataset: %s"%dataset
+print "## Will process from module %s the following samples: %s"%(options.module,  ",".join( d.name for d in datasets ) )
 
 os.system("scram runtime -sh")
 os.system("source /cvmfs/cms.cern.ch/crab3/crab.sh")
+
+## Config selection
+
+# fullsim configs
+if options.config == 'mc_80X_Summer16':
+    CMSRUN_CFG = "nano_mc_80X_Summer16"
+elif options.config == 'mc_94X_Summer16_miniAODv3':
+    CMSRUN_CFG = "nano_mc_94X_Summer16_miniAODv3"
+elif options.config == 'mc_94X_Fall17_miniAODv2':
+    CMSRUN_CFG = "nano_mc_94X_Fall17_miniAODv2"
+elif options.config == 'mc_102X_Autumn18_miniAODv1':
+    CMSRUN_CFG = "nano_mc_102X_Autumn18_miniAODv1"
+# fastsim configs
+elif options.config == 'mc_80X_fast':
+    CMSRUN_CFG = "nano_mc_fast_80X_Summer16"
+# data configs
+elif options.config == 'data_94X_Run2016':
+    CMSRUN_CFG = "nano_data_94X_Run2016"
+elif options.config == 'data_94X_Run2017':
+    CMSRUN_CFG = "nano_data_94X_Run2017"
+elif options.config == 'data_102X_Run2018_promptReco':
+    CMSRUN_CFG = "nano_data_102X_Run2018_promptReco"
+elif options.config == 'data_102X_Run2018_17Sep2018':
+    CMSRUN_CFG = "nano_data_102X_Run2018_17Sep2018"
+
+else:
+    raise NotImplementedError
+
+os.environ["CRAB_PUBLISH"]      = 'True' if options.publish else 'False'
+print "## Publication is set to", os.environ["CRAB_PUBLISH"]
+
+os.environ["CMSRUN_CFG"] = os.path.expandvars( "$CMSSW_BASE/src/Samples/cfg/%s.py" % CMSRUN_CFG )
+print "## Will use the following config: %s"%os.environ["CMSRUN_CFG"]
+
+os.system("which crab")
 
 #os.environ["CMG_REMOTE_DIR"]  = options.remoteDir
 os.environ["CRAB_UNITS_PER_JOB"] = str(options.unitsPerJob)
 if options.totalUnits:
     os.environ["CRAB_TOTAL_UNITS"] = str(options.totalUnits)
 
-m=re.match("\/(.*)\/(.*)\/(.*)",dataset)
+for dataset in datasets:
+    DASname, DAScampaign, DAStier = dataset.DASname.split('/')[1:]
 
-os.environ["CRAB_PROD_LABEL"]  = m.group(2) + "_" + options.production_label
+    os.environ["CRAB_PROD_LABEL"]  = DAScampaign + "_" + options.production_label
 
-os.environ["MAOD_SAMPLE_M1"]    = m.group(1)
-os.environ["MAOD_SAMPLE_M2"]    = m.group(2)
-os.environ["ORIG_PROD_LABEL"]   = options.production_label
-os.environ["MAOD_SAMPLE_NAME"]  = m.group(1)+"_"+m.group(2)
+    os.environ["ORIG_PROD_LABEL"]   = options.production_label
+    os.environ["MAOD_SAMPLE_NAME"]  = DASname+"_"+DAScampaign
 
-os.environ["CRAB_PUBLISH"]      = 'True' if options.publish else 'False'
-os.environ["CRAB_DATASET"]      = dataset
+    os.environ["CRAB_DATASET"]      = dataset.DASname
 
-print "## Publication is set to", os.environ["CRAB_PUBLISH"]
+    if options.dryrun:
+        print "Processing %s %s" %( dataset.name, dataset.DASname )
+        print "## Dryrun, continue..."
+        continue
 
-## Config selection
-
-# fullsim configs
-if options.era == 'mc_80X_Summer16':
-    CMSRUN_CFG = "nano_mc_80X_Summer16"
-elif options.era == 'mc_94X_Summer16_miniAODv3':
-    CMSRUN_CFG = "nano_mc_94X_Summer16_miniAODv3"
-elif options.era == 'mc_94X_Fall17_miniAODv2':
-    CMSRUN_CFG = "nano_mc_94X_Fall17_miniAODv2"
-elif options.era == 'mc_102X_Autumn18_miniAODv1':
-    CMSRUN_CFG = "nano_mc_102X_Autumn18_miniAODv1"
-# fastsim configs
-elif options.era == 'mc_80X_fast':
-    CMSRUN_CFG = "nano_mc_fast_80X_Summer16"
-# data configs
-elif options.era == 'data_94X_Run2016':
-    CMSRUN_CFG = "nano_data_94X_Run2016"
-elif options.era == 'data_94X_Run2017':
-    CMSRUN_CFG = "nano_data_94X_Run2017"
-elif options.era == 'data_102X_Run2018_promptReco':
-    CMSRUN_CFG = "nano_data_102X_Run2018_promptReco"
-elif options.era == 'data_102X_Run2018_17Sep2018':
-    CMSRUN_CFG = "nano_data_102X_Run2018_17Sep2018"
-
-else:
-    raise NotImplementedError
-
-os.environ["CMSRUN_CFG"] = os.path.expandvars( "$CMSSW_BASE/src/Samples/cfg/%s.py" % CMSRUN_CFG )
-
-print "## Will use the following config: %s"%os.environ["CMSRUN_CFG"]
-
-os.system("which crab")
-
-if options.dryrun:
-    print "## Dryrun, exiting..."
-    exit()
-
-os.system("crab submit -c crabConfig.py")
+    os.system("crab submit -c crabConfig.py")
 
---
+++ crab/launch_test.sh
@@ -0,0 +1,15 @@
+## examples to launch. Remove dryrun to actually submit to crab
+# Summer16 miniAODv3
+python launch.py --config mc_94X_Summer16_miniAODv3        --dryrun --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Summer16_miniAODv3 --sample DYJetsToLL_M50_MLM_S16_94X_ext1
+# Fall17 miniAODv2
+python launch.py --config mc_94X_Fall17_miniAODv2          --dryrun --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Fall17_miniAODv2   --sample DYJetsToLL_M10to50_HT100to200_F17_94X
+# Autumn18 miniAODvconfig
+python launch.py --config mc_102X_Autumn18_miniAODv1       --dryrun --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Autumn18_miniAODv1 --sample DYJetsToLL_M50_MLM_A18_102X
+# Run2016
+python launch.py --config data_94X_Run2016                 --dryrun --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Run2016_17Jul2018  --sample DoubleMuon_Run2016C_17Jul2018
+# Run2017
+python launch.py --config data_94X_Run2017                 --dryrun --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Run2017_31Mar2018  --sample DoubleEG_Run2017C_31Mar2018
+# Run2018 A->C
+python launch.py --config data_102X_Run2018_17Sep2018      --dryrun --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Run2018_17Sep2018  --sample DoubleMuon_Run2018B_17Sep2018_v1
+# Run2018 D
+python launch.py --config data_102X_Run2018_promptReco     --dryrun --production_label legacy_nano_v1 --remoteDir legacy_nano_v1 --unitsPerJob 2 --publish --module Run2018_promptReco --sample EGamma_Run2018D_promptReco_v2
