b24547bcdb5ca7da41eaec970805a08c8de5d532
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2019-01-08 09:54:32 +0100

    fix 2017 data cfg

--- cfg/nano_data_94X_Run2017.py
+++ cfg/nano_data_94X_Run2017.py
@@ -60,6 +60,9 @@ process.NANOAODoutput = cms.OutputModule("NanoAODOutputModule",
 # Other statements
 from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, '94X_dataRun2_v11', '')
+
+# Path and EndPath definitions
+process.nanoAOD_step = cms.Path(process.nanoSequence)
 process.endjob_step = cms.EndPath(process.endOfProcess)
 process.NANOAODoutput_step = cms.EndPath(process.NANOAODoutput)
 
