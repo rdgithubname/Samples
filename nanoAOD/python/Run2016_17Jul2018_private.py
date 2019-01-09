9f5ceefbe1b201c679cc103449069a71122de0c4
Daniel Spitzbart <daniel.spitzbart@cern.ch>
2019-01-09 16:27:49 +0100

    fix for overwrite

--- nanoAOD/python/Run2016_17Jul2018_private.py
+++ nanoAOD/python/Run2016_17Jul2018_private.py
@@ -8,18 +8,21 @@ if __name__=="__main__":
     logger = logger.get_logger("INFO", logFile = None )
     import RootTools.core.logger as logger_rt
     logger_rt = logger_rt.get_logger("INFO", logFile = None )
+
+    def get_parser():
+        import argparse
+        argParser = argparse.ArgumentParser(description = "Argument parser for samples file")
+        argParser.add_argument('--overwrite',   action='store_true',    help="Overwrite current entry in db?")
+        return argParser
+    
+    options = get_parser().parse_args()
+    ov = options.overwrite
+
 else:
     import logging
     logger = logging.getLogger(__name__)
 
-def get_parser():
-    import argparse
-    argParser = argparse.ArgumentParser(description = "Argument parser for samples file")
-    argParser.add_argument('--overwrite',   action='store_true',    help="Overwrite current entry in db?")
-    return argParser
-
-options = get_parser().parse_args()
-
+    ov = False
 
 from Samples.Tools.config import dbDir, redirector, redirector_global
 dbFile = dbDir+"DB_Run2016_17Jul2018_private.sql"
@@ -27,14 +30,14 @@ dbFile = dbDir+"DB_Run2016_17Jul2018_private.sql"
 logger.info("Using db file: %s", dbFile)
 
 # DoubleMuon
-DoubleMuon_Run2016B_26Sep2018_ver1  = Sample.nanoAODfromDAS("DoubleMuon_Run2016B_26Sep2018_ver1",   "/DoubleMuon/dspitzba-crab_Run2016B-17Jul2018_ver1-v1_legacy_nano_v3-03ee60067ceb0bfa7485a17c69df2670/USER", dbFile=dbFile, redirector=redirector, instance="phys03", overwrite=options.overwrite)
-DoubleMuon_Run2016B_26Sep2018_ver2  = Sample.nanoAODfromDAS("DoubleMuon_Run2016B_26Sep2018_ver2",   "/DoubleMuon/dspitzba-crab_Run2016B-17Jul2018_ver2-v1_legacy_nano_v3-03ee60067ceb0bfa7485a17c69df2670/USER", dbFile=dbFile, redirector=redirector, instance="phys03", overwrite=options.overwrite)
-DoubleMuon_Run2016C_26Sep2018       = Sample.nanoAODfromDAS("DoubleMuon_Run2016C_26Sep2018",        "/DoubleMuon/dspitzba-crab_Run2016C-17Jul2018-v1_legacy_nano_v3-03ee60067ceb0bfa7485a17c69df2670/USER",      dbFile=dbFile, redirector=redirector, instance="phys03", overwrite=options.overwrite)
-DoubleMuon_Run2016D_26Sep2018       = Sample.nanoAODfromDAS("DoubleMuon_Run2016D_26Sep2018",        "/DoubleMuon/dspitzba-crab_Run2016D-17Jul2018-v1_legacy_nano_v3-03ee60067ceb0bfa7485a17c69df2670/USER",      dbFile=dbFile, redirector=redirector, instance="phys03", overwrite=options.overwrite)
-DoubleMuon_Run2016E_26Sep2018       = Sample.nanoAODfromDAS("DoubleMuon_Run2016E_26Sep2018",        "/DoubleMuon/dspitzba-crab_Run2016E-17Jul2018-v1_legacy_nano_v3-03ee60067ceb0bfa7485a17c69df2670/USER",      dbFile=dbFile, redirector=redirector, instance="phys03", overwrite=options.overwrite)
-DoubleMuon_Run2016F_26Sep2018       = Sample.nanoAODfromDAS("DoubleMuon_Run2016F_26Sep2018",        "/DoubleMuon/dspitzba-crab_Run2016F-17Jul2018-v1_legacy_nano_v3-03ee60067ceb0bfa7485a17c69df2670/USER",      dbFile=dbFile, redirector=redirector, instance="phys03", overwrite=options.overwrite)
-DoubleMuon_Run2016G_26Sep2018       = Sample.nanoAODfromDAS("DoubleMuon_Run2016G_26Sep2018",        "/DoubleMuon/dspitzba-crab_Run2016G-17Jul2018-v1_legacy_nano_v3-03ee60067ceb0bfa7485a17c69df2670/USER",      dbFile=dbFile, redirector=redirector, instance="phys03", overwrite=options.overwrite)
-DoubleMuon_Run2016H_26Sep2018       = Sample.nanoAODfromDAS("DoubleMuon_Run2016H_26Sep2018",        "/DoubleMuon/dspitzba-crab_Run2016H-17Jul2018-v1_legacy_nano_v3-03ee60067ceb0bfa7485a17c69df2670/USER",      dbFile=dbFile, redirector=redirector, instance="phys03", overwrite=options.overwrite)
+DoubleMuon_Run2016B_17Jul2018_ver1  = Sample.nanoAODfromDAS("DoubleMuon_Run2016B_17Jul2018_ver1",   "/DoubleMuon/dspitzba-crab_Run2016B-17Jul2018_ver1-v1_legacy_nano_v3-03ee60067ceb0bfa7485a17c69df2670/USER", dbFile=dbFile, redirector=redirector, instance="phys03", overwrite=ov)
+DoubleMuon_Run2016B_17Jul2018_ver2  = Sample.nanoAODfromDAS("DoubleMuon_Run2016B_17Jul2018_ver2",   "/DoubleMuon/dspitzba-crab_Run2016B-17Jul2018_ver2-v1_legacy_nano_v3-03ee60067ceb0bfa7485a17c69df2670/USER", dbFile=dbFile, redirector=redirector, instance="phys03", overwrite=ov)
+DoubleMuon_Run2016C_17Jul2018       = Sample.nanoAODfromDAS("DoubleMuon_Run2016C_17Jul2018",        "/DoubleMuon/dspitzba-crab_Run2016C-17Jul2018-v1_legacy_nano_v3-03ee60067ceb0bfa7485a17c69df2670/USER",      dbFile=dbFile, redirector=redirector, instance="phys03", overwrite=ov)
+DoubleMuon_Run2016D_17Jul2018       = Sample.nanoAODfromDAS("DoubleMuon_Run2016D_17Jul2018",        "/DoubleMuon/dspitzba-crab_Run2016D-17Jul2018-v1_legacy_nano_v3-03ee60067ceb0bfa7485a17c69df2670/USER",      dbFile=dbFile, redirector=redirector, instance="phys03", overwrite=ov)
+DoubleMuon_Run2016E_17Jul2018       = Sample.nanoAODfromDAS("DoubleMuon_Run2016E_17Jul2018",        "/DoubleMuon/dspitzba-crab_Run2016E-17Jul2018-v1_legacy_nano_v3-03ee60067ceb0bfa7485a17c69df2670/USER",      dbFile=dbFile, redirector=redirector, instance="phys03", overwrite=ov)
+DoubleMuon_Run2016F_17Jul2018       = Sample.nanoAODfromDAS("DoubleMuon_Run2016F_17Jul2018",        "/DoubleMuon/dspitzba-crab_Run2016F-17Jul2018-v1_legacy_nano_v3-03ee60067ceb0bfa7485a17c69df2670/USER",      dbFile=dbFile, redirector=redirector, instance="phys03", overwrite=ov)
+DoubleMuon_Run2016G_17Jul2018       = Sample.nanoAODfromDAS("DoubleMuon_Run2016G_17Jul2018",        "/DoubleMuon/dspitzba-crab_Run2016G-17Jul2018-v1_legacy_nano_v3-03ee60067ceb0bfa7485a17c69df2670/USER",      dbFile=dbFile, redirector=redirector, instance="phys03", overwrite=ov)
+DoubleMuon_Run2016H_17Jul2018       = Sample.nanoAODfromDAS("DoubleMuon_Run2016H_17Jul2018",        "/DoubleMuon/dspitzba-crab_Run2016H-17Jul2018-v1_legacy_nano_v3-03ee60067ceb0bfa7485a17c69df2670/USER",      dbFile=dbFile, redirector=redirector, instance="phys03", overwrite=ov)
 
 
 
@@ -44,14 +47,14 @@ DoubleMuon_Run2016H_26Sep2018       = Sample.nanoAODfromDAS("DoubleMuon_Run2016H
 
 
 DoubleMuon = [
-    DoubleMuon_Run2016B_26Sep2018_ver1,
-    DoubleMuon_Run2016B_26Sep2018_ver2,
-    DoubleMuon_Run2016C_26Sep2018,
-    DoubleMuon_Run2016D_26Sep2018,
-    DoubleMuon_Run2016E_26Sep2018,
-    DoubleMuon_Run2016F_26Sep2018,
-    DoubleMuon_Run2016G_26Sep2018,
-    DoubleMuon_Run2016H_26Sep2018,
+    DoubleMuon_Run2016B_17Jul2018_ver1,
+    DoubleMuon_Run2016B_17Jul2018_ver2,
+    DoubleMuon_Run2016C_17Jul2018,
+    DoubleMuon_Run2016D_17Jul2018,
+    DoubleMuon_Run2016E_17Jul2018,
+    DoubleMuon_Run2016F_17Jul2018,
+    DoubleMuon_Run2016G_17Jul2018,
+    DoubleMuon_Run2016H_17Jul2018,
 ]
 
 MuonEG = [
