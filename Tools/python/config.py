8845a5d650e1120edb0b98b1d7932c9d5eb5d2b0
Lukas Lechner <llechner@users.noreply.github.com>
2019-02-18 12:13:24 +0100

    update config.py for users.
    This will also be needed for conor submission

--- Tools/python/config.py
+++ Tools/python/config.py
@@ -1,14 +1,16 @@
 '''
 Some definitions.
-To avoid changes of the file being pushed to github, e.g. when you define a different dbDir, use:
-git update-index --skip-worktree config.py
 '''
 import os
 
 ## This definition might not work for everybody. Update with whatever you like.
-dbDir               = '/afs/hephy.at/data/%s01/nanoAOD/'%os.environ['USER']
-redirector          = 'root://hephyse.oeaw.ac.at/'
-redirector_global   = 'root://cms-xrd-global.cern.ch/'
+if os.environ['USER'] in ['llechner']:
+    dbDir = '/afs/hephy.at/data/llechner01/TTGammaEFT/cache/samples/'
+else:
+    dbDir = '/afs/hephy.at/data/%s01/nanoAOD/'%os.environ['USER']
+
+redirector        = 'root://hephyse.oeaw.ac.at/'
+redirector_global = 'root://cms-xrd-global.cern.ch/'
 
 if not os.path.isdir(dbDir): os.makedirs(dbDir)
 
