7acfe37731fc7dab39c4d8aac86dffc543f2ccd4
priyasajid <priyasajid@hotmail.com>
2019-05-13 11:10:51 +0200

    priya db directory

--- Tools/python/config.py
+++ Tools/python/config.py
@@ -8,6 +8,8 @@ if os.environ['USER'] in ['llechner']:
     dbDir = '/afs/hephy.at/data/llechner01/TTGammaEFT/cache/samples/'
 elif os.environ['USER'] in ['dspitzba', 'dspitzbart']:
     dbDir = '/afs/hephy.at/data/dspitzbart01/nanoAOD/'
+elif os.environ['USER'] in ['phussain']:
+    dbDir = '/afs/hephy.at/data/dspitzbart01/nanoAOD/'
 else:
     dbDir = '/afs/hephy.at/data/%s01/nanoAOD/'%os.environ['USER']
 
