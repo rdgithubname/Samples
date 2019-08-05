22ef163abd0ed542602a1e8bfdabe689f80f3835
Lukas Lechner <lukas.k.lechner@gmail.com>
2019-08-05 18:34:08 +0200

    update launch scripts for miniAOD production

--- production/run_Summer16_miniAODv3.sh
+++ production/run_Summer16_miniAODv3.sh
@@ -1,5 +1,5 @@
 # General settings
-events=200
+events=1000
 fragment=pythiaFragmentCUEP8M1_LO_0123j
 gridpack=ZAToLLA0123j_5f_LO_MLM_slc6_amd64_gcc630_CMSSW_9_3_8
 gridpackDir=/afs/hephy.at/data/llechner01/TTGammaEFT/gridpacks/VGamma_LO/ZGamma/
