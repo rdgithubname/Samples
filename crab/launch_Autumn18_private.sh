fd6db3a6ea403f3e398355adfea170225d0c65e7
Lukas Lechner <lukas.k.lechner@gmail.com>
2019-08-30 11:51:33 +0200

    launch WJets samples

--- crab/launch_Autumn18_private.sh
+++ crab/launch_Autumn18_private.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # TTGamma private samples
-#python launch.py --runOnLocal --config nano_mc_102X_Autumn18_miniAODv1 --production_label legacy_nano_v2 --remoteDir legacy_nano_v2 --unitsPerJob 20 --publish --module Autumn18_private --sample allSamples
-python launch.py --runOnLocal --config nano_mc_102X_Autumn18_miniAODv1 --production_label legacy_nano_v2 --remoteDir legacy_nano_v2 --unitsPerJob 20 --publish --module Autumn18_private --sample TTGamma_dilep_LO_A18_private #SPLIT15
-python launch.py --runOnLocal --config nano_mc_102X_Autumn18_miniAODv1 --production_label legacy_nano_v2 --remoteDir legacy_nano_v2 --unitsPerJob 20 --publish --module Autumn18_private --sample TTGamma_semilep_LO_A18_private #SPLIT15
-python launch.py --runOnLocal --config nano_mc_102X_Autumn18_miniAODv1 --production_label legacy_nano_v2 --remoteDir legacy_nano_v2 --unitsPerJob 20 --publish --module Autumn18_private --sample TTGamma_had_LO_A18_private #SPLIT15
+#python launch.py $@ --sorted --runOnLocal --config nano_mc_102X_Autumn18_miniAODv1 --production_label legacy_nano_v3 --remoteDir legacy_nano_v3 --unitsPerJob 40 --publish --module Autumn18_private --sample allSamples
+#python launch.py $@ --sorted --runOnLocal --config nano_mc_102X_Autumn18_miniAODv1 --production_label legacy_nano_v3 --remoteDir legacy_nano_v3 --unitsPerJob 40 --publish --module Autumn18_private --sample TTGamma_dilep_LO_A18_private #SPLIT20
+#python launch.py $@ --sorted --runOnLocal --config nano_mc_102X_Autumn18_miniAODv1 --production_label legacy_nano_v3 --remoteDir legacy_nano_v3 --unitsPerJob 40 --publish --module Autumn18_private --sample TTGamma_semilep_LO_A18_private #SPLIT20
+#python launch.py $@ --sorted --runOnLocal --config nano_mc_102X_Autumn18_miniAODv1 --production_label legacy_nano_v3 --remoteDir legacy_nano_v3 --unitsPerJob 40 --publish --module Autumn18_private --sample TTGamma_had_LO_A18_private #SPLIT20
+python launch.py $@ --sorted --runOnLocal --config nano_mc_102X_Autumn18_miniAODv1 --production_label legacy_nano_v3 --remoteDir legacy_nano_v3 --unitsPerJob 50 --publish --module Autumn18_private --sample TTGamma_nofullyhad_LO_A18_private #SPLIT20
