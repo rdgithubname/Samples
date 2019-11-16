26bc1aef1e959e1b6e2d1a3a986708c77be09700
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2019-11-16 23:35:18 +0100

    sample copy

--- Tools/python/AutoClass.py
+++ Tools/python/AutoClass.py
@@ -1,12 +1,61 @@
 ''' class that stores all arguments as attributes
 '''
+        
+import os
+import subprocess
+from multiprocessing import Pool
+
+def copy( job ):
+    source, target = job
+    cmd = ["/usr/bin/rfcp",  source, target]
+    dir = os.path.dirname( target )
+    if not os.path.exists(dir):
+        #try:
+            os.makedirs(dir)
+        #except:
+        #    pass
+            
+    if not os.path.exists(target):
+        logger.info( " ".join( cmd ) )
+        subprocess.call(cmd)
+    else:
+        logger.info( "Found %s. Skip.", target)
+
+def echo( job ):
+    source, target = job
+    cmd = ["/usr/bin/xrdcp", source, target ]
+    logger.info( " ".join( cmd ) )
+
+# Logger
+import logging
+logger = logging.getLogger(__name__)
 
 class AutoClass:
     def __init__( self, samples):
         for s in samples:
             setattr( self, s.name, s )
 
+        self.__samples = samples
+
         names = [ s.name for s in samples ]
         for name in names:
             if names.count( name )>1:
                 raise RuntimeError( "Sample name is not unique: %s", name )
+
+    def copy_to_local( self, path = "/data/dpm/", n_processes = 4, do_it = False):
+        if not path.endswith('/'): path+='/'
+        jobs = []
+        for sample in self.__samples:
+            for source in sample.files:
+                _source = os.path.join( "/dpm/oeaw.ac.at/home/cms", ("".join( source.split('//')[2:])).lstrip('/') )
+                _target = os.path.join( path, ("".join( source.split('//')[2:])).lstrip('/') )
+                jobs.append( [_source, _target] )
+                #logger.info( "Copying %s to %s"%( source, target ) )
+
+        pool = Pool(processes = n_processes)
+        if do_it:
+            results = pool.map(copy, jobs)
+        else:
+            results = pool.map(echo, jobs)
+        pool.close()
+        pool.join()
