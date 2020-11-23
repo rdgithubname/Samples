517a5f520d65955d217ecfd8ac5176f2f4169d4f
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2020-11-23 16:17:29 +0100

    launch_nanoAOD_with_PFCands.sh

---
+++ crab/nanoAOD/launch_nanoAOD_with_PFCands.sh
@@ -0,0 +1,3 @@
+python launch.py --config nano_v6_mc_10218_Summer16_NANO_PFCands  --dryrun --production_label v6_PFCands --remoteDir v6_PFCands --unitsPerJob 2 --publish --module Summer16_miniAODv3 --sample DYJetsToLL_M50_LO_ext1
+python launch.py --config nano_v6_mc_10218_Summer16_NANO_PFCands  --dryrun --production_label v6_PFCands --remoteDir v6_PFCands --unitsPerJob 2 --publish --module Summer16_miniAODv3 --sample QCD_Mu_pt30to50
+
